1a6beaca228adf44fe403da8386cb388421f9aec
Spimy <officialspyme@gmail.com>
2023-10-01 22:26:28 +0800

    📦 NEW: Add solution to practical 2 q3

---
+++ 02_Practical/q3.py
@@ -0,0 +1,21 @@
+from utils import input_num
+
+def question_3():
+  yearly_income = input_num('Enter your yearly income: ')
+
+  # Set the tax rate based on yearly income
+  if yearly_income < 2_500:
+    tax_rate = 0
+  
+  elif 2_501 <= yearly_income <= 10_000:
+    tax_rate = 0.05
+
+  elif 10_001 <= yearly_income <= 50_000:
+    tax_rate = 0.15
+
+  else:
+    tax_rate = 0.25
+
+  # Calculate total tax in a single so it is easier to refactor code
+  total_tax = yearly_income * tax_rate
+  print(f'Total tax: RM{total_tax:.2f}')
