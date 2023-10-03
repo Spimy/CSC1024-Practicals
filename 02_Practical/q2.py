21c27c078e5d8c1ecba8f1b51f58b87e29b6fd15
Spimy <officialspyme@gmail.com>
2023-10-03 14:56:50 +0800

    👌 IMPROVE: Justify with 2 digits

--- 02_Practical/q2.py
+++ 02_Practical/q2.py
@@ -1,10 +1,12 @@
 from utils import input_num
 
+
 def question_2():
-  time_elapsed_seconds = int(input_num('Enter number of seconds lapsed for event: '))
+    time_elapsed_seconds = int(
+        input_num('Enter number of seconds lapsed for event: '))
 
-  hours = time_elapsed_seconds // 3600
-  minutes = (time_elapsed_seconds % 3600) // 60
-  time_elapsed_seconds %= 3600 // 60
+    hours = time_elapsed_seconds // 3600
+    minutes = (time_elapsed_seconds % 3600) // 60
+    time_elapsed_seconds %= 3600 // 60
 
-  print(f'{hours}:{minutes}:{time_elapsed_seconds}')
+    print(f'{hours:0>2}:{minutes:0>2}:{time_elapsed_seconds:0>2}')
