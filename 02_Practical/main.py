215bfd14d1a84b4a22150e1f1bd5940071f34326
Spimy <officialspyme@gmail.com>
2023-10-03 13:15:15 +0800

    📦 NEW: Add option to select question 7

--- 02_Practical/main.py
+++ 02_Practical/main.py
@@ -5,18 +5,22 @@ from q3 import question_3
 from q4 import question_4
 from q5 import question_5
 from q6 import question_6
+from q7 import question_7
+
 
 def main():
-  question_num = int(input_num('Enter a question number: '))
+    question_num = int(input_num('Enter a question number: '))
 
-  match(question_num):
-    case 1: question_1() 
-    case 2: question_2()
-    case 3: question_3()
-    case 4: question_4()
-    case 5: question_5()
-    case 6: question_6()
-    case _: main()
+    match(question_num):
+        case 1: question_1()
+        case 2: question_2()
+        case 3: question_3()
+        case 4: question_4()
+        case 5: question_5()
+        case 6: question_6()
+        case 7: question_7()
+        case _: main()
+
 
 if __name__ == '__main__':
-  main()
+    main()
