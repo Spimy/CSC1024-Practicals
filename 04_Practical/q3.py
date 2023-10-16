e5ff325f1812ddcf2b79014f8dc4ab2a7a881fd3
Spimy <officialspyme@gmail.com>
2023-10-16 15:39:03 +0800

    📦 NEW: Add solution to question 3 for practical 4

---
+++ 04_Practical/q3.py
@@ -0,0 +1,15 @@
+from utils import input_num
+
+
+def question_3():
+    a = int(input_num('Enter first number (a): '))
+    b = int(input_num('Enter second number (b): '))
+
+    i = 0
+    ans = a
+
+    while i < b:
+        ans *= a
+        i += 1
+
+    print('a^b =', ans)
