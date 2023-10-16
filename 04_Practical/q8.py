3183e1f52535c32f28ac98f880b86d7692bfc009
Spimy <officialspyme@gmail.com>
2023-10-16 16:38:30 +0800

    📦 NEW: Add solution to extra question 3 for practical 4

---
+++ 04_Practical/q8.py
@@ -0,0 +1,19 @@
+from utils import input_num
+
+
+def question_8():
+    square_size = int(input_num('Enter size of square: '))
+
+    i = 0
+    while i < square_size:
+        j = 0
+
+        while j < square_size:
+            if 0 < i < square_size - 1 and 0 < j < square_size - 1:
+                print(' ', end=' ')
+            else:
+                print('*', end=' ')
+            j += 1
+
+        i += 1
+        print()
