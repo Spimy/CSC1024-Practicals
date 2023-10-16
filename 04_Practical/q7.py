52b5573ca96843f8aa7e6740eb71b43851e1b58d
Spimy <officialspyme@gmail.com>
2023-10-16 16:25:55 +0800

    📦 NEW: Add solution to extra question 2 for practical 4

---
+++ 04_Practical/q7.py
@@ -0,0 +1,15 @@
+# d = 0.5g(t^2)
+def question_7():
+    time = 10  # in seconds
+    acceleration = 32  # in ft/sec^2
+
+    i = 0
+    cumulative_distance = 0
+
+    print(f'{"Time":<9} {"Interval Distance"} {"Total Distance":>20}')
+    while i < time + 1:
+        interval_distance = 0.5 * acceleration * (i ** 2)
+        cumulative_distance += interval_distance
+        print(f'{i:<20} {interval_distance:>6} {cumulative_distance:>20}')
+
+        i += 1
