f7ae75c7b23f06a32ab808a824a2d8c01723e92a
Spimy <officialspyme@gmail.com>
2023-10-16 20:51:29 +0800

    👌 IMPROVE: Add square size constraint

--- 04_Practical/q8.py
+++ 04_Practical/q8.py
@@ -2,7 +2,10 @@ from utils import input_num
 
 
 def question_8():
-    square_size = int(input_num('Enter size of square: '))
+    square_size = 0
+
+    while square_size < 1 or square_size > 20:
+        square_size = int(input_num('Enter size of square: '))
 
     i = 0
     while i < square_size:
