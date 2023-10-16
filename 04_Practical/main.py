5ab8305cfd64ffd356c1de56d5051346fcfefc1d
Spimy <officialspyme@gmail.com>
2023-10-16 16:39:01 +0800

    📦 NEW: Add main file to run questions

---
+++ 04_Practical/main.py
@@ -0,0 +1,28 @@
+from utils import input_num
+from q1 import question_1
+from q2 import question_2
+from q3 import question_3
+from q4 import question_4
+from q5 import question_5
+from q6 import question_6
+from q7 import question_7
+from q8 import question_8
+
+
+def main():
+    question_num = int(input_num('Enter a question number: '))
+
+    match(question_num):
+        case 1: question_1()
+        case 2: question_2()
+        case 3: question_3()
+        case 4: question_4()
+        case 5: question_5()
+        case 6: question_6()
+        case 7: question_7()
+        case 8: question_8()
+        case _: main()
+
+
+if __name__ == '__main__':
+    main()
