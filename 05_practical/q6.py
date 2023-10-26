64f3c0c82542ac76d10941018688802f67433565
Spimy <officialspyme@gmail.com>
2023-10-26 23:51:11 +0800

    📦 NEW: Add solution for practical 5 extra question 2

---
+++ 05_practical/q6.py
@@ -0,0 +1,51 @@
+from utils import input_num
+
+
+def question_6():
+    characters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
+    while True:
+        selection = ''
+
+        while selection not in ('encode', 'decode'):
+            selection = input(
+                "Type 'encode' to encrypt, type 'decode' to decrypt: ")
+
+        msg = input('Type your message: ')
+        shift = int(input_num('Type the shift number: '))
+
+        i = 0
+        result = ''
+
+        while i < len(msg):
+            try:
+                if selection == 'decode':
+                    new_index = characters.index(msg[i]) - shift
+                    if new_index < 0:
+                        new_index = new_index + len(characters)
+                else:
+                    new_index = characters.index(msg[i]) + shift
+                    if new_index >= len(characters):
+                        new_index = new_index - len(characters)
+
+                result += characters[new_index]
+            except ValueError:
+                result += msg[i]
+
+            i += 1
+
+        print(f'The {selection}d text is {result}')
+
+        cont = ''
+
+        while cont.lower() not in ('yes', 'no'):
+            cont = input(
+                "Type 'yes' if you want to go again. Otherwise, type 'no': "
+            )
+
+        if cont.lower() == 'no':
+            break
+        else:
+            # Print a new line before restarting
+            print()
