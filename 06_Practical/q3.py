07ba82163d99431c4035ca0300129b4f3eb9a6a8
Spimy <officialspyme@gmail.com>
2023-11-06 22:57:07 +0800

    👌 IMPROVE: Do not use reversed function

--- 06_Practical/q3.py
+++ 06_Practical/q3.py
@@ -5,4 +5,10 @@ def question_3():
     # Again... I didn't want to repeat code so I've gone ahead used what was returned in q1
     forward_list = question_1()
     print('Forward list: ', forward_list)
-    print('Reverse list: ', list(reversed(forward_list)))
+
+    # Ok fine I did not read that it said do not use built-in functions except append and sort
+    # print('Reverse list: ', list(reversed(forward_list)))
+
+    # Just following the rules :)
+    forward_list.sort(reverse=True)
+    print('Reverse list: ', forward_list)
