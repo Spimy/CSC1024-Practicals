e3e60329b32c61bda6f5c9ebfe4ec483f53aa0f3
Spimy <officialspyme@gmail.com>
2023-11-06 22:44:47 +0800

    📦 NEW: Add solution for practical 6 question 1

---
+++ 06_Practical/q1.py
@@ -0,0 +1,14 @@
+from utils import input_num
+
+
+def question_1():
+    i = 0
+    my_list = []
+
+    while i < 5:
+        num = input_num('Enter a number: ')
+        my_list.append(num)
+        i += 1
+
+    my_list.sort()
+    print(my_list)
